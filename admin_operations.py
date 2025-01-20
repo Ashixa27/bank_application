a0a3e07b9fd673cd929db5be6d023452e6f97773
Damaris Grecu <damaris_grecu@yahoo.com>
2025-01-20 21:38:24 +0200

    added new function

--- admin_operations.py
+++ admin_operations.py
@@ -1,9 +1,39 @@
 import json
+from locale import currency
 
 
 def remove_user(user_to_be_deleted: str, bank_path: str = "bank.json", auth_path: str = "auth.json" , clients_path: str = "clients.json"):
 
-# make this more efficient
+    file_paths = {
+        "accounts": bank_path,
+        "credentials": auth_path,
+        "clients": clients_path
+    }
+    for key, path in file_paths.items():
+        with open(path, "r") as f:
+            key = json.loads(f.read())
+
+        key.pop(user_to_be_deleted, None)
+
+        with open(path, "w") as f:
+            f.write(json.dumps(key, indent=4))
+
+
+def add_user(user_to_add: str, bank_path: str = "bank.json", auth_path: str = "auth.json" , clients_path: str = "clients.json"):
+
+    user_details = {
+        "name": 0,
+        "telefon": 0,
+        "oras": 0
+    }
+    for k, v in user_details.items():
+        user_details.update({k:input(f"{k}: ")})
+
+    bank_details = {
+        "value": 0,
+        "currency": 0
+    }
+    bank_details.update({"currency":input("currency: ")})
 
     with open(bank_path, "r") as f:
         accounts = json.loads(f.read())
@@ -14,9 +44,9 @@ def remove_user(user_to_be_deleted: str, bank_path: str = "bank.json", auth_path
     with open(clients_path, "r") as f:
         clients = json.loads(f.read())
 
-    accounts.pop(user_to_be_deleted, None)
-    credentials.pop(user_to_be_deleted, None)
-    clients.pop(user_to_be_deleted, None)
+    accounts.update({user_to_add: bank_details})
+    credentials.update({user_to_add: input("Setati o parola: ")})
+    clients.update({user_to_add: user_details})
 
     with open(bank_path, "w") as f:
         f.write(json.dumps(accounts, indent=4))
@@ -26,11 +56,3 @@ def remove_user(user_to_be_deleted: str, bank_path: str = "bank.json", auth_path
 
     with open(clients_path, "w") as f:
         f.write(json.dumps(clients, indent=4))
-
-
-
-
-
-
-def add_user():
-    pass
